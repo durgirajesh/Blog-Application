e4ab5b9c7d133e338edc88671893bcd6adb4b15f
durgirajesh <durgirajesh@gmail.com>
2024-04-30 23:04:59 +0530

    created posts model

---
+++ blog/posts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class post_model(models.Model):
+    title = models.TextField(max_length=10)
+    content = models.TextField(max_length=1000)
+    createdAtTime = models.TimeField(auto_now_add=True)
+    createdAtDate = models.DateField(auto_now_add=True)
+    updatedAtTime = models.TimeField(auto_now=True)
+    updatedAtDate = models.DateField(auto_now=True)
---
+++ blog/posts/views.py
@@ -0,0 +1,2 @@
+from .models import post_model 
+from .forms import post_form
